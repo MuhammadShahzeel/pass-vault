19a3379eed2a32a008fa052722e6d2e571ee72c2
shahzeel <shazeelarain786@gmail.com>
2025-04-04 20:07:34 +0500

    added toggle eye

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,7 +1,21 @@
-import { FaEye, FaPlus } from "react-icons/fa6";
+import { useState } from "react";
+import { FaEye, FaPlus,FaEyeSlash } from "react-icons/fa6";
 import { MdOutlineVpnKey } from "react-icons/md";
 
 const Manager = () => {
+  const [toggleEye, settoggleEye] = useState(true);
+  const showPassword = () => {
+    settoggleEye(!toggleEye);
+   
+
+    
+  };
+  const savePassword = () => {
+    
+   
+
+    
+  };
   return (
     <div className="flex-1 flex items-center justify-center bg-slate-900 pt-10">
       <div className="w-full max-w-4xl bg-slate-800 rounded-xl shadow-2xl overflow-hidden border border-slate-700/50">
@@ -32,15 +46,20 @@ const Manager = () => {
                   placeholder="Enter Password"
                   className="w-full bg-slate-700 rounded-lg px-5 py-4 text-white placeholder-slate-400 focus:outline-none focus:ring-2 focus:ring-green-400 transition duration-200 border border-slate-600/20"
                 />
-                <span className="absolute right-4 top-1/2 transform -translate-y-1/2 text-slate-400 hover:text-green-400 cursor-pointer transition duration-200">
-                  <FaEye className="text-xl" />
+                <span onClick={showPassword} className="absolute right-4 top-1/2 transform -translate-y-1/2 text-slate-400 hover:text-green-400 cursor-pointer transition duration-200">
+                 {toggleEye ?  <FaEye className="text-xl" /> :  <FaEyeSlash className="text-xl" />}
+                 
+                 
+                 
                 </span>
               </div>
             </div>
           </div>
                  
           <div className="flex justify-center mt-8">
-            <button className="flex items-center gap-2 bg-green-400 hover:bg-green-500 text-slate-900 font-medium py-3 px-8 rounded-lg transition duration-200 shadow-lg hover:shadow-xl active:bg-green-600 active:translate-y-0.5">
+            <button onClick={savePassword}  className="flex items-center gap-2 bg-green-400 hover:bg-green-500 text-slate-900 font-medium py-3 px-8 rounded-lg transition duration-200 shadow-lg hover:shadow-xl active:bg-green-600 active:translate-y-0.5">
+              
+              {}
               <FaPlus size={16} />
               Add Password
             </button>
